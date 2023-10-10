adfcb793d668a2c1345597b128428a0d78c940eb
Valdo_Meditech <androsbgr13@gmail.com>
2023-10-10 18:59:42 +0800

    feat : code player solve

--- Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/BE2_Ins_MoveForward.cs
+++ Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/BE2_Ins_MoveForward.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using MG_BlocksEngine2.Block.Instruction;
 using MG_BlocksEngine2.Block;
+using JetBrains.Annotations;
 
 public class BE2_Ins_MoveForward : BE2_InstructionBase, I_BE2_Instruction
 {
@@ -24,12 +25,20 @@ public class BE2_Ins_MoveForward : BE2_InstructionBase, I_BE2_Instruction
     //{
     //    
     //}
-
-    public new void Function()
+   /* public new void Function()
     {
         _input0 = Section0Inputs[0];
-        _value = _input0.FloatValue;
+        _value = _input0.FloatValue; // melakukan penambahan bilangan float.
         TargetObject.Transform.position += TargetObject.Transform.forward * _value;
         ExecuteNextInstruction();
-    }
+    }*/
+    public new void Function()
+    {
+   
+            _input0 = Section0Inputs[0];
+            _value = _input0.FloatValue; // melakukan penambahan bilangan float. ( tapi tidak jadi gagal)
+            TargetObject.Transform.position += TargetObject.Transform.forward * _value;
+            ExecuteNextInstruction();
+        
+}
 }
